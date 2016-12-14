972a37b5dec13daae695a6b056a21c5b80f5c45d
kikei <fujii@xaxxi.net>
2016-12-15 03:16:01 +0900

    added listening default port in server.js

--- src/server.js
+++ src/server.js
@@ -4,4 +4,5 @@ var http = require('http');
 var app = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<h1>Hello, world!</h1>');
-}).listen(3001);
+}).listen(process.env.PORT || 3001);
+
